6fb4e1779a1c94b7656ef0f0253e8e819750b026
Murillo Andrade <murillodesu@gmail.com>
2018-09-10 20:12:05 -0300

    Atividade dia da semana

---
+++ aula-3/semana/br/ufg/inf/mf/praticas/Aplicacao.java
@@ -0,0 +1,8 @@
+package br.ufg.inf.mf.praticas;
+import java.time.LocalDate;
+public class Aplicacao{
+
+public static void main(String[] args) {
+System.out.println(LocalDate.now().getDayOfWeek());
+ }
+}
