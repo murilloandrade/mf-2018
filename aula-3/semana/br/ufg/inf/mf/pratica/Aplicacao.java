19eb9da8e90a3ba162a3fa5fb3a6c15c03e31b1d
Murillo Andrade <murillodesu@gmail.com>
2018-09-10 20:44:14 -0300

    Correcao no nome da pasta, readicionando pasta

---
+++ aula-3/semana/br/ufg/inf/mf/pratica/Aplicacao.java
@@ -0,0 +1,8 @@
+package br.ufg.inf.mf.pratica;
+import java.time.LocalDate;
+public class Aplicacao{
+
+public static void main(String[] args) {
+System.out.println(LocalDate.now().getDayOfWeek());
+ }
+}
