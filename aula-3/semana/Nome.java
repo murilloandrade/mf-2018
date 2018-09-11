e10fe2598dc9c7c7ad7f1cd5343236d5644844f1
Murillo Andrade <murillodesu@gmail.com>
2018-09-10 21:24:58 -0300

    Exercicio nome, setando LOCALE

---
+++ aula-3/semana/Nome.java
@@ -0,0 +1,10 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class Nome{
+
+    public static void main(String[] args) {
+        System.out.println(LocalDate.now().format(DateTimeFormatter.ofPattern("EEEE",Locale.getDefault())));
+    }
+}
