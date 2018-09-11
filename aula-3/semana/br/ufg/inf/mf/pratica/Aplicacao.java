f3749d1e8feebcebe47802cdb0c21a5cec7022a8
Murillo Andrade <murillodesu@gmail.com>
2018-09-10 21:37:37 -0300

    algumas identações em aplicacao.java

--- aula-3/semana/br/ufg/inf/mf/pratica/Aplicacao.java
+++ aula-3/semana/br/ufg/inf/mf/pratica/Aplicacao.java
@@ -1,8 +1,11 @@
 package br.ufg.inf.mf.pratica;
-import java.time.LocalDate;
-public class Aplicacao{
 
-public static void main(String[] args) {
-System.out.println(LocalDate.now().getDayOfWeek());
- }
+import java.time.LocalDate;
+
+public class Aplicacao
+{
+	public static void main(String[] args)
+        {
+		System.out.println(LocalDate.now().getDayOfWeek());
+        }
 }
