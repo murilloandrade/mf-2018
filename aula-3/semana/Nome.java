5f095fc24110b8f7c581585aa10b166f38081d65
Murillo Andrade <murillodesu@gmail.com>
2018-09-10 21:32:59 -0300

    algumas identações em nome.java

--- aula-3/semana/Nome.java
+++ aula-3/semana/Nome.java
@@ -2,9 +2,10 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
-public class Nome{
-
-    public static void main(String[] args) {
+public class Nome
+{
+    public static void main(String[] args)
+    {
         System.out.println(LocalDate.now().format(DateTimeFormatter.ofPattern("EEEE",Locale.getDefault())));
     }
 }
